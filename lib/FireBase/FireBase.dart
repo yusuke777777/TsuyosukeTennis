84df49abac81fb8862671ee1d376b47a05099914
yusuke777777 <yusuke2370@gmail.com>
2022-06-22 00:48:43 +0900

    活動場所複数登録処理追加

--- lib/FireBase/FireBase.dart
+++ lib/FireBase/FireBase.dart
@@ -5,10 +5,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
 
 import '../Common/CprofileSetting.dart';
+import '../Common/CactivityList.dart';
 
 class FirestoreMethod {
   static FirebaseFirestore _firestoreInstance = FirebaseFirestore.instance;
-  static final pedmeterRef = _firestoreInstance.collection('myProfile');
+  static final profileRef = _firestoreInstance.collection('myProfile');
 
   static final Firebase_Auth.FirebaseAuth auth =
       Firebase_Auth.FirebaseAuth.instance;
@@ -20,20 +21,31 @@ class FirestoreMethod {
     String today = outputFormat.format(now);
 
     try {
-      await pedmeterRef.doc(auth.currentUser!.uid).set({
+      await profileRef.doc(auth.currentUser!.uid).set({
         'USER_ID': auth.currentUser!.uid,
         'PROFILE_IMAGE': profile.PROFILE_IMAGE,
         'NICK_NAME': profile.NICK_NAME,
         'TOROKU_RANK': profile.TOROKU_RANK,
-        'TODOFUKEN':profile.activityList,
+        // 'TODOFUKEN': profile.activityList,
         'AGE': profile.AGE,
         'COMENT': profile.COMENT,
         'koushinYmd': today,
       });
     } catch (e) {
-      print('ユーザー登録に失敗しました --- $e');
+      print('ユーザー登録に失敗しましたあ --- $e');
     }
-    
+
+    profile.activityList.forEach((a) async {
+      try {
+        await profileRef
+            .doc(auth.currentUser!.uid)
+            .collection("activityList")
+            .doc("ActivityNo" + a.No)
+            .set({'TODOFUKEN': a.TODOFUKEN, 'SHICHOSON': a.SHICHOSON.text});
+      } catch (e) {
+        print('ユーザー登録に失敗しました --- $e');
+      }
+    });
   }
 
 //   //各ユーザー歩数情報の取得
