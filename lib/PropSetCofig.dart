c1b9eef271286e819f7ad9f07454f367a6f4b14b
飯崎剛志 <iizakitsuyoshi@hansakitsuyoshinoMacBook-Air.local>
2023-02-05 00:29:54 +0900

    “検索結果画面デザイン修正”

--- lib/PropSetCofig.dart
+++ lib/PropSetCofig.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'Common/CtalkRoom.dart';
+import 'FireBase/FireBase.dart';
 import 'Page/FriendManagerPage.dart';
+import 'Page/ProfileReference.dart';
+import 'Page/TalkRoom.dart';
 
 /**
  * ヘッダ部の共通クラスです
@@ -43,5 +47,51 @@ class DrawerConfig {
         ),
       ),
     );
+  }
+}
+
+/**
+ * 検索結果リストの共通化クラスです
+ */
+class ListTileConfig {
+  static late ListTile listTile;
+
+  void init(BuildContext context, String name, String profile, String docId, String loginUserId) {
+    listTile = ListTile(
+      tileColor: Colors.white24,
+      leading: ClipOval(
+        child: GestureDetector(
+          //アイコン押下時の挙動
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProfileReference(docId),
+                ));
+          },
+          child: profile == ""
+              ? Image.asset('images/upper_body-2.png',
+              fit: BoxFit.cover)
+              : Image.network(
+            profile,
+            width: 70,
+            height: 70,
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+      title: Text(name,
+          style: TextStyle(fontSize: 20)),
+      //リスト押下時の挙動
+      onTap: () async {
+        TalkRoomModel room = await FirestoreMethod.makeRoom(loginUserId, docId);
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TalkRoom(room),
+            ));
+      },
+    );
+
   }
 }
