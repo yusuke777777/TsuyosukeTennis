6be8b84eda7847c7dffdb8fae61be135d38d74a8
飯崎剛志 <iizakitsuyoshi@hansakitsuyoshinoMacBook-Air.local>
2023-08-27 19:15:07 +0900

    “課題あり_称号対応”

--- lib/Common/CprofileSetting.dart
+++ lib/Common/CprofileSetting.dart
@@ -10,6 +10,7 @@ class CprofileSetting{
   late String GENDER;
   late String COMENT;
   late String MY_USER_ID;
+  Map<String, String>? TITLE;
 
-  CprofileSetting({required this.USER_ID,required this.PROFILE_IMAGE,required this.NICK_NAME,required this.TOROKU_RANK,required this.activityList,required this.AGE,required this.GENDER,required this.COMENT,required this.MY_USER_ID});
+  CprofileSetting({required this.USER_ID,required this.PROFILE_IMAGE,required this.NICK_NAME,required this.TOROKU_RANK,required this.activityList,required this.AGE,required this.GENDER,required this.COMENT,required this.MY_USER_ID,this.TITLE});
 }
---
+++ lib/Page/MyTitlePage.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../Common/CTitle.dart';
+import '../FireBase/FireBase.dart';
+import '../PropSetCofig.dart';
+import 'package:yaml/yaml.dart';
+
+class MyTitlePage extends StatelessWidget {
+  // Title.yamlに記載されている全ての称号を取得
+  late List<CTitle> titles = [];
+  late CTitle title;
+
+  /**
+   * Title.yamlに記載されている称号を全て取得
+   */
+  Future<List<CTitle>> loadTitles() async {
+    final String yamlString = await rootBundle.loadString('assets/Title.yaml');
+    final List<dynamic> yamlList = loadYaml(yamlString);
+    Map<String, dynamic> map = await FirestoreMethod.getMyTitle();
+    for (var item in yamlList) {
+      String name = item['name'];
+      String description = item['description'];
+      title = CTitle(name: name, description: description, status: map[name].toString());
+      titles.add(title);
+    }
+    return titles;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future<List<CTitle>> futureList = loadTitles();
+    HeaderConfig().init(context, "取得称号一覧");
+    DrawerConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: HeaderConfig.backGroundColor,
+        title: HeaderConfig.appBarText,
+          iconTheme: IconThemeData(color: Colors.black),
+          leading: HeaderConfig.backIcon
+      ),
+      body: FutureBuilder(
+          future: futureList,
+          builder:
+              (BuildContext context, AsyncSnapshot<List<CTitle>> snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return new Align(
+                  child: Center(
+                child: new CircularProgressIndicator(),
+              ));
+            } else if (snapshot.hasError) {
+              return new Text('Error!!: ${snapshot.error!}');
+            } else if (snapshot.hasData) {
+              List<CTitle>? titleList = snapshot.data;
+              return ListView.builder(
+                itemCount: titleList?.length,
+                itemBuilder: (context, index) {
+                  final title = titles[index];
+                  return ListTile(
+                    title: Text(title.name),
+                    subtitle: Text(title.description),
+                    tileColor:
+                    title.status == "1" ?
+                    Colors.white : Colors.grey
+                  );
+                },
+              );
+            } else {
+              return Text("データが存在しません");
+            }
+          }),
+    );
+  }
+}
